6fda7cb2b361f56b97c4dfa97ed100702071cca5
jmike-cpu <78622196+jmike-cpu@users.noreply.github.com>
2021-04-13 20:48:16 +0300

    auth.py,base.html

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods=['GET','POST'])
-def login():
+def login():  
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -12,6 +12,17 @@ def logout():
 
 @auth.route('/signUp', methods=['GET','POST'])
 def signUp():
+    if request.method =='POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len (password1) !=password2:
+            flash("passwords do not much .",category='error')
+        elif len(password1) < 7:
+            flash('password must be at least 7 characters. ',category='error')
+
     return render_template("signUp.html")
 
 
